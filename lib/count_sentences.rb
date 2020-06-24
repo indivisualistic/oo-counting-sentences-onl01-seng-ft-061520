c336493af4f37c5864e29d07e742d317bee94528
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-24 18:52:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
+
 
   end
 
   def count_sentences
+    self.split(/\.|\?|\!/).delete_if do |s|
 
-  end
+      s.length <2
+
+    end.count
+
+end
 end
